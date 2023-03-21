f18579cee774d353d6d36ad7d2302a2f2a6687d3
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2023-03-21 01:58:00 -0300

    Configuração sqlit

--- core/settings.py
+++ core/settings.py
@@ -103,9 +103,13 @@ if os.environ.get('DB_ENGINE') and os.environ.get('DB_ENGINE') == "mysql":
     }
 else:
     DATABASES = {
-        "default":dj_database_url.config(default='sqlite:///db.sqlite3',conn_max_age=600),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
